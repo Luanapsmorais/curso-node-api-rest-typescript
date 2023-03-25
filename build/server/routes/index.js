541cb8922593abd43d9b54aed804bb93f2ff1dee
Luana Morais <lm082891@gmail.com>
2023-03-25 15:50:56 -0300

    feat: add jest lib, change tsconfig

--- build/server/routes/index.js
+++ build/server/routes/index.js
@@ -1,16 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.router = void 0;
-// Router é um middleware, onde escifico todas as rotas
 const express_1 = require("express");
-const http_status_codes_1 = require("http-status-codes");
+const controllers_1 = require("../controllers");
 const router = (0, express_1.Router)();
 exports.router = router;
 router.get("/", (request, response) => {
-    return response.send("Olá, Luana!");
+    return response.send("Hello, Luana!");
 });
-// teste de outra rota - funcionou!
-router.post("/teste", (request, response) => {
-    console.log(request.body);
-    return response.status(http_status_codes_1.StatusCodes.UNAUTHORIZED).json(request.body);
-});
+router.get("/cities", controllers_1.CitiesController.getAllValidation, controllers_1.CitiesController.getAll);
+router.post("/cities", controllers_1.CitiesController.createValidation, controllers_1.CitiesController.create);
+router.get("/cities/:id", controllers_1.CitiesController.getByIdValidation, controllers_1.CitiesController.getById);
+router.put("/cities/:id", controllers_1.CitiesController.updateByIdValidation, controllers_1.CitiesController.updateById);
+router.delete("/cities/:id", controllers_1.CitiesController.deleteByIdValidation, controllers_1.CitiesController.deleteById);
